59c73b450d3ac0e3e55f96e081d096416afbd1fd
lumap <55940580+lumap@users.noreply.github.com>
2020-05-15 09:23:04 +0200

    Update help.js

--- src/commands/help.js
+++ src/commands/help.js
@@ -2,19 +2,43 @@ module.exports = {
     name: 'help',
     aliases: ['link','links','vote','invite','support'],
     async execute(client,message) {
-        message.channel.send({embed: {
-            color: 0xffff00,
-            title: "Pichu help menu",
-            thumbnail: {
-                url: client.user.avatarURL({format: 'png',size: 2048})
-            },
-            description: `
-                List of commands: [here](https://pichu.lumap.me/)
-                Vote for the bot: [here](https://top.gg/bot/674497635171696644/vote)
-                Invite the bot: [here](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot)
-                Support server: [here](https://discord.gg/uWzaaEK)
-                Donate: [here](https://www.patreon.com/lumap)
-                `
-        }})
+            if (args[0]) {
+            return getCMD(client, message, args[0]);
+        } else {
+            return getAll(client, message);
+        }
     }
+}
+
+function getAll(client, message) {
+    const embed = new Discord.MessageEmbed()
+
+        .setColor("RANDOM") 
+    const commands = (category) => {
+        return client.commands
+            .filter(cmd => cmd.category === category)
+            .map(cmd => `\`${cmd.name}\``)
+            .join(", ");
+    }
+
+    const info = client.categories 
+        .map(cat => stripIndents`**${cat[0].toUpperCase() + cat.slice(1)}** \n${commands(cat)}`)
+        .reduce((string, category) => string + "\n" + category);
+
+    return message.channel.send(embed.setDescription(info));
+}
+//random color already exists
+function getCMD(client, message, input) {
+    const embed = new Discord.MessageEmbed()
+    if (!client.commands.get(input.toLowerCase())){ 
+        if (!client.aliases.get(input.toLowerCase())) return message.channel.send(`No information found for command **${input.toLowerCase()}**`)
+    } //done
+    const cmd = client.commands.get(input.toLowerCase()) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))
+
+    if (cmd.name) info = `**Command name**: ${cmd.name}`;
+    if (cmd.aliases) info += `\n**Aliases**: ${cmd.aliases.map(a => `\`${a}\``).join(", ")}`;
+    }
+
+    return message.channel.send(embed.setColor("GREEN").setDescription(info).setFooter('Syntax: <> = required, [] = optional'));
+}    }
 }
