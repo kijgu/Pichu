638eefbb8902fc85bc0fb7cabf20c1eca375e3d3
lumap <55940580+lumap@users.noreply.github.com>
2020-05-15 10:06:33 +0200

    Update help.js

--- src/commands/help.js
+++ src/commands/help.js
@@ -14,18 +14,9 @@ function getAll(client, message) {
     const embed = new Discord.MessageEmbed()
 
         .setColor("RANDOM") 
-    const commands = (category) => {
-        return client.commands
-            .filter(cmd => cmd.category === category)
-            .map(cmd => `\`${cmd.name}\``)
-            .join(", ");
-    }
+  
 
-    const info = client.categories 
-        .map(cat => stripIndents`**${cat[0].toUpperCase() + cat.slice(1)}** \n${commands(cat)}`)
-        .reduce((string, category) => string + "\n" + category);
-
-    return message.channel.send(embed.setDescription(info));
+    return message.channel.send(embed.setDescription(client.commands.map(c => `\`\`${c.name}\`\``));
 }
 //random color already exists
 function getCMD(client, message, input) {
